64fbbc874c502e722f2978b21142414ab12a2c53
Berkay Bideci <berkaybideci@gmail.com>
2023-05-12 19:34:55 +0300

    v1.5

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,38 @@
 window.addEventListener("DOMContentLoaded", () => {
-  async function fetchData() {
-    const response = await fetch(
-      "https://random-word-api.herokuapp.com/word?number=1"
-    );
-    const data = await response.json();
-    console.log(data[0].split(""));
+    async function fetchData() {
+        const response = await fetch(
+            "https://random-word-api.herokuapp.com/word?number=1"
+        );
+        const data = await response.json();
+        console.log(data[0].split(""));
 
-    const wordLoop = data[0].split("");
-    for (let letter of wordLoop) {
-      const blankSpan = document.createElement("span");
-      const letterDiv = document.querySelector("#letter-div");
-      blankSpan.innerText = "_ ";
-      letterDiv.appendChild(blankSpan);
-    }
-
-    const letterButtons = document.querySelectorAll("main button");
-    // console.log(letterButtons)
-    letterButtons.forEach((button) => {
-      button.addEventListener("click", letterFunction);
-    });
-
-    function letterFunction(e) {
-      // console.log(e.target.innerText)
-      for (let letter of data[0]) {
-        console.log(letter); //tek tek tüm harfler
-
-        if (letter.includes(e.target.innerText)) {
-          //   let indexLetter = data[0].indexOf(letter);
-          const blankSpans = document.querySelectorAll("span");
-          for (let blankSpan of blankSpans) {
-            if (
-              Array.from(blankSpans).indexOf(blankSpan) ===
-              data[0].indexOf(letter)
-            ) {
-              blankSpan.innerText = e.target.innerText;
-            }
-          }
+        const letterArray = data[0].split("");
+        for (let letter of letterArray) {
+            const blankSpan = document.createElement("span");
+            const letterDiv = document.querySelector("#letter-div");
+            blankSpan.innerText = "_ ";
+            letterDiv.appendChild(blankSpan);
         }
-      }
-    }
-  }
 
-  fetchData();
+        const letterButtons = document.querySelectorAll("main button");
+        letterButtons.forEach((button) => {
+            button.addEventListener("click", letterFunction);
+        });
+
+        const liveCounter = document.querySelector("#live-counter")
+
+        function letterFunction(e) {
+            const blankSpans = document.querySelectorAll("span")
+            if (letterArray.includes(e.target.innerText)) {
+                letterArray.forEach((letter, index) => {
+                    if (letter === e.target.innerText) {
+                        blankSpans[index].innerText = letter
+                        e.target.disabled = true
+                    }
+                })
+            }
+        }
+    }
+
+    fetchData();
 });
